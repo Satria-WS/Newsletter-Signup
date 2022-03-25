d66d0a055bde5712b1c549e3be197d9b2f27117d
Satria-WS <extencezone02@gmail.com>
2022-03-26 00:32:43 +0700

    post finish

--- app.js
+++ app.js
@@ -2,18 +2,82 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https")
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+
 
 const app = express();
 
-//create path static folder , for keep css or other files
 
+
+//create body-parser, for grab the data from the sign up form
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+//create path static folder , for keep css or other files
 app.use(express.static("public"));
+//listening port 3000
+app.listen(process.env.PORT||3000, () => {
+  console.log("server is running on port 3000");
+});
+
 
 //create home route 
-app.get("/" , (req,res)=> {
-   res.sendFile(__dirname + "/signup.html")
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/signup.html")
+});
+
+// Setting up MailChimp
+mailchimp.setConfig({
+    apiKey: "737ee8835f3f138146613adf9c5a94e0-us14",
+    server: "us14"
 })
 
-app.listen(3000, ()=> {
-    console.log("server is running on port 3000");
-})
+//create post route
+app.post("/", (req, res) => {
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+
+    const listId = "c4fc82ed35";
+    //creating object with the users data
+    const subscribingUser = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+    };
+
+    //uploading data to server
+    async function callPing() {
+        const response = await mailchimp.lists.addListMember(listId, {
+            email_address: subscribingUser.email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: subscribingUser.firstName,
+                LNAME: subscribingUser.lastName,
+            }
+        });
+
+        res, sendFile(__dirname + "/success.html");
+        console.log(
+            `Successfully added contact as an audience member. The contact's id is ${response.id}`
+        );
+    }
+
+    console.log(firstName , lastName , email);
+    callPing().catch(e => res.sendFile(__dirname + "/failure.html"));
+});
+
+
+
+
+
+
+
+
+
+
+
+//API key: 737ee8835f3f138146613adf9c5a94e0-us14
+//List Id : c4fc82ed35
